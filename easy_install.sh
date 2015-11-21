10c84fba9ff6e936a95b58ddb4e57488901778cd
Caio Moreno de Souza <caiomsouza@gmail.com>
2015-11-21 15:48:02 +0100

    updated

---
+++ easy_install.sh
@@ -0,0 +1,5 @@
+sudo apt-get -y install unzip git
+sudo mkdir pentaho
+sudo wget http://sourceforge.net/projects/pentaho/files/Data%20Integration/5.4/pdi-ce-5.4.0.1-130.zip/download
+sudo unzip download
+
