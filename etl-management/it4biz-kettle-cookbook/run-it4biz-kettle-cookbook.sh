bc444cfb55a275b92b6995bc8e150735ba9edbfd
Caio Moreno de Souza <caiomsouza@gmail.com>
2015-11-28 15:09:53 +0100

    Add KettleCookbook

---
+++ etl-management/it4biz-kettle-cookbook/run-it4biz-kettle-cookbook.sh
@@ -0,0 +1,6 @@
+echo "Run IT4biz Kettle Cookbook"
+
+cd /Applications/Pentaho/5.4.0/pdi-ce-5.4.0.1-130/data-integration
+
+
+sh kitchen.sh -file:/Applications/Pentaho/5.4.0/it4biz-kettle-cookbook/pdi/document-folder.kjb -param:"INPUT_DIR"=/Users/caiomsouza/git/github.com/pdi-automatic-news-collector/ -param:"OUTPUT_DIR"=/Users/caiomsouza/git/github.com/pdi-automatic-news-collector/it4biz-kettle-cookbook-output/ -param:"SAXON"=./lib/saxon-9.1.0.8.jar
