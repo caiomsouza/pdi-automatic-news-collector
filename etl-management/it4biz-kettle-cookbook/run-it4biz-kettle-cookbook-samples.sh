243d0ba4e3f9b4da3cafa4207a2b3307b8ce4b9f
Caio Moreno de Souza <caiomsouza@gmail.com>
2015-11-28 15:36:40 +0100

    melhorias

--- etl-management/it4biz-kettle-cookbook/run-it4biz-kettle-cookbook-samples.sh
+++ etl-management/it4biz-kettle-cookbook/run-it4biz-kettle-cookbook-samples.sh
@@ -3,4 +3,4 @@ echo "Run IT4biz Kettle Cookbook"
 cd /Applications/Pentaho/5.4.0/pdi-ce-5.4.0.1-130/data-integration
 
 
-sh kitchen.sh -file:/Applications/Pentaho/5.4.0/it4biz-kettle-cookbook/pdi/document-folder.kjb -param:"INPUT_DIR"=/Applications/Pentaho/5.4.0/pdi-ce-5.4.0.1-130/data-integration/samples/ -param:"OUTPUT_DIR"=/Users/caiomsouza/git/github.com/pdi-automatic-news-collector/it4biz-kettle-cookbook-output-samples/ -param:"SAXON"=./lib/saxon-9.1.0.8.jar
+sh kitchen.sh -file:/Applications/Pentaho/5.4.0/it4biz-kettle-cookbook/pdi/document-folder.kjb -param:"INPUT_DIR"=/Applications/Pentaho/5.4.0/pdi-ce-5.4.0.1-130/data-integration/samples/ -param:"OUTPUT_DIR"=/Users/caiomsouza/git/github.com/pdi-automatic-news-collector/etl-management/it4biz-kettle-cookbook/it4biz-kettle-cookbook-output-samples/ -param:"SAXON"=./lib/saxon-9.1.0.8.jar
